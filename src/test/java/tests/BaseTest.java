af096e95c454f5f97e0bc12dd7b4684ec5b79dbf
Evsin Igor <Evsin19@icloud.com>
2024-05-27 00:51:51 +0300

    Добавленны параметризованные тесты

--- src/test/java/tests/BaseTest.java
+++ src/test/java/tests/BaseTest.java
@@ -4,7 +4,6 @@ import com.codeborne.selenide.Configuration;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import static com.codeborne.selenide.Selenide.closeWebDriver;
-import static com.codeborne.selenide.Selenide.open;
 
 public class BaseTest {
     @BeforeEach
